13d50cc9055c1880a92fbfb7e44deb3573a35e82
livalves <liviamabelle@gmail.com>
2024-06-07 23:04:18 -0300

    Adicionando tela de planejamento - Finalizando app

--- lib/modules/planning/screens/planning_screen.dart
+++ lib/modules/planning/screens/planning_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import '../../../widgets/menu_navigator.dart'; 
+import '../../../widgets/menu_navigator.dart';
 import '../../add/screens/expense_screen.dart';
 
 class PlanningScreen extends StatefulWidget {
@@ -10,7 +10,7 @@ class PlanningScreen extends StatefulWidget {
 }
 
 class _PlanningScreenState extends State<PlanningScreen> {
-  int currentTab = 3; 
+  int currentTab = 1;
 
   void changeTab(int index) {
     setState(() {
@@ -21,20 +21,139 @@ class _PlanningScreenState extends State<PlanningScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Conteúdo da tela de planejamento'),
+      appBar: AppBar(
+        title: Text(
+          'Planejamento',
+          style: TextStyle(color: Colors.white),
+          textAlign: TextAlign.center,
+        ),
+        centerTitle: true,
+        automaticallyImplyLeading: false,
+        backgroundColor: Color.fromRGBO(6, 88, 246, 1),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              padding: EdgeInsets.all(16.0),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                border: Border.all(color: Color.fromRGBO(6, 88, 246, 1), width: 2.0),
+                borderRadius: BorderRadius.circular(8.0), 
+              ),
+              child: Row(
+                children: [
+                  Icon(Icons.account_balance_wallet, size: 32.0, color: Color.fromRGBO(6, 88, 246, 1)),
+                  SizedBox(width: 16.0),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('Reserva de Emergência', style: TextStyle(fontWeight: FontWeight.bold)),
+                        SizedBox(height: 8.0),
+                        LinearProgressIndicator(
+                          value: 0.5, 
+                          backgroundColor: Colors.grey[300],
+                          valueColor: AlwaysStoppedAnimation<Color>(Color.fromRGBO(6, 88, 246, 1)),
+                        ),
+                        SizedBox(height: 4.0),
+                        Text('R\$ 1.000,00', style: TextStyle(fontSize: 14.0)),
+                      ],
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.edit),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 16.0),
+            Container(
+              padding: EdgeInsets.all(16.0),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                border: Border.all(color: Color.fromRGBO(6, 88, 246, 1), width: 2.0),
+                borderRadius: BorderRadius.circular(8.0), 
+              ),
+              child: Row(
+                children: [
+                  Icon(Icons.airplanemode_active, size: 32.0, color: Color.fromRGBO(6, 88, 246, 1)),
+                  SizedBox(width: 16.0),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('Viagem - Recife', style: TextStyle(fontWeight: FontWeight.bold)),
+                        SizedBox(height: 8.0),
+                        LinearProgressIndicator(
+                          value: 0.7, 
+                          backgroundColor: Colors.grey[300],
+                          valueColor: AlwaysStoppedAnimation<Color>(Color.fromRGBO(6, 88, 246, 1)),
+                        ),
+                        SizedBox(height: 4.0),
+                        Text('R\$ 550,00', style: TextStyle(fontSize: 14.0)),
+                      ],
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.edit),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 16.0),
+            Container(
+              padding: EdgeInsets.all(16.0),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                border: Border.all(color: Color.fromRGBO(6, 88, 246, 1), width: 2.0),
+                borderRadius: BorderRadius.circular(8.0), 
+              ),
+              child: Row(
+                children: [
+                  Icon(Icons.directions_car, size: 32.0, color: Color.fromRGBO(6, 88, 246, 1)),
+                  SizedBox(width: 16.0),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('Carro (Entrada)', style: TextStyle(fontWeight: FontWeight.bold)),
+                        SizedBox(height: 8.0),
+                        LinearProgressIndicator(
+                          value: 0.2, 
+                          backgroundColor: Colors.grey[300],
+                          valueColor: AlwaysStoppedAnimation<Color>(Color.fromRGBO(6, 88, 246, 1)),
+                        ),
+                        SizedBox(height: 4.0),
+                        Text('R\$ 20.000,00', style: TextStyle(fontSize: 14.0)),
+                      ],
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.edit),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
       floatingActionButton: IconButton(
         icon: Image.asset(
-                '../../assets/icons/add.png',
-                width: 64,
-                height: 64,
-              ),
+          '../../assets/icons/add.png',
+          width: 64,
+          height: 64,
+        ),
         onPressed: () {
           Navigator.push(
-          context,
-          MaterialPageRoute(builder: (context) => ExpenseScreen()),
-        );
+            context,
+            MaterialPageRoute(builder: (context) => ExpenseScreen()),
+          );
         },
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
@@ -43,6 +162,60 @@ class _PlanningScreenState extends State<PlanningScreen> {
         onTabChanged: changeTab,
       ),
       backgroundColor: Color.fromARGB(255, 237, 235, 226),
+    );
+  }
+
+  Widget _buildLegendItem(String title, Color color) {
+    return Row(
+      children: [
+        Container(
+          width: 16,
+          height: 16,
+          color: color,
+        ),
+        SizedBox(width: 8.0),
+        Text(title, style: TextStyle(color: Color.fromRGBO(6, 88, 246, 1), fontSize: 15)),
+      ],
+    );
+  }
+}
+
+class TransactionItem extends StatelessWidget {
+  final IconData icon;
+  final String title;
+  final String subtitle;
+  final String amount;
+
+  TransactionItem({
+    required this.icon,
+    required this.title,
+    required this.subtitle,
+    required this.amount,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(8.0),
+        side: BorderSide(color: Color.fromRGBO(6, 88, 246, 1)),
+      ),
+      child: ListTile(
+        leading: Icon(icon, color: Color.fromRGBO(6, 88, 246, 1)),
+        title: Text(
+          title,
+          style: TextStyle(color: Color.fromRGBO(6, 88, 246, 1)),
+        ),
+        subtitle: Text(subtitle),
+        trailing: Text(
+          amount,
+          style: TextStyle(
+            color: Color.fromRGBO(6, 88, 246, 1),
+            fontWeight: FontWeight.bold,
+            fontSize: 15,
+          ),
+        ),
+      ),
     );
   }
 }
